805b60303d538c43b60f4fd6b33730137c4f39f1
Sarvesh Ahuja <sahuja@in5762.et.local>
2015-07-01 16:29:52 -0400

    third commit

--- init/index.php
+++ init/index.php
@@ -1,8 +1,51 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+  <head>
+    <title>Salesforce Marketing Cloud - Welcome to the Fuel Platform</title>
+    <link rel="stylesheet" type="text/css" href="./stylesheet.css">
+  </head>
+  <body>
+    <table width="100%">
+      <tr>
+        <td bgcolor="#7C868D" height="150" width="100%">
+          <table align="center" width="80%">
+            <tr>
+              <td align="left"><img src="http://image.s6.exacttarget.com/lib/fe9112727360007b77/m/1/SFMC_logo.png" style="width: 50%;"></td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td bgcolor="#E98300" height="60" width="100%"></td>
+      </tr>
+    </table>
+    <table width="60%" align="center">
+      <tr>
+        <td align="center"><br>
+          <h1>Welcome to the Fuel Platform</h1>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">This application contains the Auth, REST, and SOAP Node.js libraries for the Salesforce Marketing Cloud's Fuel Platform.<br>See the links below for additional detail:</td>
+      </tr>
+      <tr>
+        <td align="center"><br><br>Learn more about our Fuel Platform Node Auth, REST, and SOAP libraries along with documentation and examples:<br></td>
+      </tr>
+      <tr>
+        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/Fuel-Node-Auth">https://github.com/ExactTarget/Fuel-Node-Auth</a></td>
+      </tr>
+      <tr>
+        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/Fuel-Node-REST">https://github.com/ExactTarget/Fuel-Node-REST</a></td>
+      </tr>
+      <tr>
+        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/Fuel-Node-SOAP">https://github.com/ExactTarget/Fuel-Node-SOAP</a></td>
+      </tr>
+      <tr>
+        <td align="center"><br>For step-by-step instructions on creating a Salesforce Marketing Cloud ClientID and ClientSecret, visit:</td>
+      </tr>
+      <tr>
+        <td align="center"><a target="_blank" href="http://code.exacttarget.com/apis-sdks/rest-api/using-app-center-to-get-an-api-key.html">Salesforce Marketing Cloud - App Center</a></td>
+      </tr>
+    </table>
+  </body>
 </html>
