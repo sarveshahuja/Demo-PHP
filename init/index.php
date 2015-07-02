810b8d7991d5480c0e81db773f8acc2f79fa0e5d
Sarvesh Ahuja <sahuja@in5762.local>
2015-07-02 10:25:35 -0400

    modifying webpage

--- init/index.php
+++ init/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
+  
   <head>
     <title>Salesforce Marketing Cloud - Welcome to the Fuel Platform</title>
     <link rel="stylesheet" type="text/css" href="./stylesheet.css">
   </head>
+  
   <body>
     <table width="100%">
       <tr>
         <td bgcolor="#7C868D" height="150" width="100%">
-          <table align="center" width="80%">
+          <table align="center" width="100%">
             <tr>
               <td align="left"><img src="http://image.s6.exacttarget.com/lib/fe9112727360007b77/m/1/SFMC_logo.png" style="width: 50%;"></td>
             </tr>
@@ -19,30 +21,34 @@
         <td bgcolor="#E98300" height="60" width="100%"></td>
       </tr>
     </table>
+    
     <table width="60%" align="center">
       <tr>
         <td align="center"><br>
           <h1>Welcome to the Fuel Platform</h1>
         </td>
       </tr>
+      
       <tr>
-        <td align="center">This application contains the Auth, REST, and SOAP Node.js libraries for the Salesforce Marketing Cloud's Fuel Platform.<br>See the links below for additional detail:</td>
+        <td align="center">This application contains the PHP SDK for the Salesforce Marketing Cloud's Fuel Platform, and provides seamless access to our API services (including REST and SOAP).</td>
       </tr>
+      
       <tr>
-        <td align="center"><br><br>Learn more about our Fuel Platform Node Auth, REST, and SOAP libraries along with documentation and examples:<br></td>
+        <td align="center"><br><br>Learn more about our Fuel Platform SDKs along with documentation and examples:<br></td>
       </tr>
+      
       <tr>
-        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/Fuel-Node-Auth">https://github.com/ExactTarget/Fuel-Node-Auth</a></td>
+        <td align="center"><a target="_blank" href="https://code.exacttarget.com/apis-sdks/fuel-sdks/">Salesforce Marketing Cloud's Fuel SDKs</a></td>
       </tr>
+      
       <tr>
-        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/Fuel-Node-REST">https://github.com/ExactTarget/Fuel-Node-REST</a></td>
+        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/FuelSDK-PHP">Fuel PHP SDK - Github Repository</a></td>
       </tr>
+      
       <tr>
-        <td align="center"><a target="_blank" href="https://github.com/ExactTarget/Fuel-Node-SOAP">https://github.com/ExactTarget/Fuel-Node-SOAP</a></td>
+        <td align="center"><br>For step-by-step instructions on creating a Salesforce Marketing Cloud's ClientID/ClientSecret and API Key, please visit:</td>
       </tr>
-      <tr>
-        <td align="center"><br>For step-by-step instructions on creating a Salesforce Marketing Cloud ClientID and ClientSecret, visit:</td>
-      </tr>
+      
       <tr>
         <td align="center"><a target="_blank" href="http://code.exacttarget.com/apis-sdks/rest-api/using-app-center-to-get-an-api-key.html">Salesforce Marketing Cloud - App Center</a></td>
       </tr>
